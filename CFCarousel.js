fc4a74137d495fe5c2f71c7d24abf7c975f6f4d8
animotionguy <38657686+animotionguy@users.noreply.github.com>
2019-08-16 18:43:19 -0500

    Create CFCarousel.js
    Base Carousel Script

---
+++ CFCarousel.js
@@ -0,0 +1,43 @@
+(function ( $ ) {
+  $.fn.cfCarousel = function ( carouselsettings ) {
+
+    var settings = $.extend( {}, $.fn.cfUnslider.defaults, carouselsettings );
+
+    return this.each(function () {
+		$(this).find(settings.slides).wrap("<li></li>");
+		$(this).unslider(
+	      {
+	        selectors: {
+	          container: settings.container,
+	          slides: 'li'
+	        },
+	        autoplay: settings.autoplay,
+	        infinite: settings.infinite,
+	        arrows: settings.arrows,
+	        delay: settings.delay,
+	        animation: settings.animation,
+	        nav: settings.nav
+	      });
+
+		$(this).on("mouseover",function () {
+			$(this).unslider('stop');
+		});
+
+		$(this).on("mouseout",function () {
+			$(this).unslider('start');
+		});
+	});
+  }
+
+  $.fn.cfCarousel.defaults = {
+    container: ".col-inner",
+    slides: ".elImageWrapper",
+    autoplay: true,
+    infinite: true,
+    arrows: false,
+    delay: 3000,
+    animation: "horizontal",
+    nav: false
+  };
+}
+(jQuery));
